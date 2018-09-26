a9f89833071eeb9155724a947d678a63dc7234bb
Neil Lee <neil.lee@udngroup.com.tw>
2018-09-27 07:37:18 +0800

    新增 - 吳恩達機器學習課程的Python實作

---
+++ 20180927/04_stanford-machine-learning/kmeans/test_bi_kmeans.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+# kmeans/test_bi_kmeans.py
+
+import kmeans
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    dataMat = np.mat(kmeans.loadDataSet('data/testSet2.txt'))
+    centroids, clusterAssment = kmeans.biKmeans(dataMat, 3)
+    clusterCount = centroids.shape[0]
+    m = dataMat.shape[0]
+    # 绘制散点图
+    patterns = ['o', 'D', '^']
+    colors = ['b', 'g', 'y']
+    fig = plt.figure()
+    title = 'bi-kmeans with k=3'
+    ax = fig.add_subplot(111, title=title)
+    for k in range(clusterCount):
+        # 绘制聚类中心
+        ax.scatter(centroids[k,0], centroids[k,1], color='r', marker='+', linewidth=20)
+        for i in range(m):
+            # 绘制属于该聚类中心的样本
+            ptsInCluster = dataMat[np.nonzero(clusterAssment[:, 0].A==k)[0]]
+            ax.scatter(ptsInCluster[:, 0].flatten().A[0], ptsInCluster[:, 1].flatten().A[0], marker=patterns[k], color=colors[k])
+    plt.show()
---
+++ 20180927/04_stanford-machine-learning/linear_regression/regression.py
@@ -0,0 +1,247 @@
+# coding: utf-8
+# linear_regression/regression.py
+import numpy as np
+import matplotlib as plt
+import time
+
+def exeTime(func):
+    """ 耗时计算装饰器
+    """
+    def newFunc(*args, **args2):
+        t0 = time.time()
+        back = func(*args, **args2)
+        return back, time.time() - t0
+    return newFunc
+
+def loadDataSet(filename):
+    """ 读取数据
+
+    从文件中获取数据，在《机器学习实战中》，数据格式如下
+    "feature1 TAB feature2 TAB feature3 TAB label"
+
+    Args:
+        filename 文件名
+
+    Returns:
+        X 训练样本集矩阵
+        y 标签集矩阵
+    """
+    numFeat = len(open(filename).readline().split('\t')) - 1
+    X = []
+    y = []
+    file = open(filename)
+    for line in file.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        X.append(lineArr)
+        y.append(float(curLine[-1]))
+    return np.mat(X), np.mat(y).T
+
+def h(theta, x):
+    """预测函数
+
+    Args:
+        theta 相关系数矩阵
+        x 特征向量
+
+    Returns:
+        预测结果
+    """
+    return (theta.T*x)[0,0]
+
+def J(theta, X, y):
+    """代价函数
+
+    Args:
+        theta 相关系数矩阵
+        X 样本集矩阵
+        y 标签集矩阵
+
+    Returns:
+        预测误差（代价）
+    """
+    m = len(X)
+    return (X*theta-y).T*(X*theta-y)/(2*m)
+
+@exeTime
+def bgd(rate, maxLoop, epsilon, X, y):
+    """批量梯度下降法
+
+    Args:
+        rate 学习率
+        maxLoop 最大迭代次数
+        epsilon 收敛精度
+        X 样本矩阵
+        y 标签矩阵
+
+    Returns:
+        (theta, errors, thetas), timeConsumed
+    """
+    m,n = X.shape
+    # 初始化theta
+    theta = np.zeros((n,1))
+    count = 0
+    converged = False
+    error = float('inf')
+    errors = []
+    thetas = {}
+    for j in range(n):
+        thetas[j] = [theta[j,0]]
+    while count<=maxLoop:
+        if(converged):
+            break
+        count = count + 1
+        for j in range(n):
+            deriv = (y-X*theta).T*X[:, j]/m
+            theta[j,0] = theta[j,0]+rate*deriv
+            thetas[j].append(theta[j,0])
+        error = J(theta, X, y)
+        errors.append(error[0,0])
+        # 如果已经收敛
+        if(error < epsilon):
+            converged = True
+    return theta,errors,thetas
+
+@exeTime
+def sgd(rate, maxLoop, epsilon, X, y):
+    """随机梯度下降法
+    Args:
+        rate 学习率
+        maxLoop 最大迭代次数
+        epsilon 收敛精度
+        X 样本矩阵
+        y 标签矩阵
+    Returns:
+        (theta, error, thetas), timeConsumed
+    """
+    m,n = X.shape
+    # 初始化theta
+    theta = np.zeros((n,1))
+    count = 0
+    converged = False
+    error = float('inf')
+    errors = []
+    thetas = {}
+    for j in range(n):
+        thetas[j] = [theta[j,0]]
+    while count <= maxLoop:
+        if converged:
+            break
+        count = count + 1
+        errors.append(float('inf'))
+        for i in range(m):
+            if converged:
+                break
+            diff = y[i,0]-h(theta, X[i].T)
+            for j in range(n):
+                theta[j,0] = theta[j,0] + rate*diff*X[i, j]
+                thetas[j].append(theta[j,0])
+            error = J(theta, X, y)
+            errors[-1] = error[0,0]
+            # 如果已经收敛
+            if(error < epsilon):
+                converged = True
+    return theta, errors, thetas
+
+def JLwr(theta, X, y, x, c):
+    """局部加权线性回归的代价函数计算式
+
+    Args:
+        theta 相关系数矩阵
+        X 样本集矩阵
+        y 标签集矩阵
+        x 待预测输入
+        c tau
+    Returns:
+        预测代价
+    """
+    m,n = X.shape
+    summerize = 0
+    for i in range(m):
+        diff = (X[i]-x)*(X[i]-x).T
+        w = np.exp(-diff/(2*c*c))
+        predictDiff = np.power(y[i] - X[i]*theta,2)
+        summerize = summerize + w*predictDiff
+    return summerize
+
+@exeTime
+def lwr(rate, maxLoop, epsilon, X, y, x, c=1):
+    """局部加权线性回归
+
+    Args:
+        rate 学习率
+        maxLoop 最大迭代次数
+        epsilon 预测精度
+        X 输入样本
+        y 标签向量
+        x 待预测向量
+        c tau
+    """
+    m,n = X.shape
+    # 初始化theta
+    theta = np.zeros((n,1))
+    count = 0
+    converged = False
+    error = float('inf')
+    errors = []
+    thetas = {}
+    for j in range(n):
+        thetas[j] = [theta[j,0]]
+    # 执行批量梯度下降
+    while count<=maxLoop:
+        if(converged):
+            break
+        count = count + 1
+        for j in range(n):
+            deriv = (y-X*theta).T*X[:, j]/m
+            theta[j,0] = theta[j,0]+rate*deriv
+            thetas[j].append(theta[j,0])
+        error = JLwr(theta, X, y, x, c)
+        errors.append(error[0,0])
+        # 如果已经收敛
+        if(error < epsilon):
+            converged = True
+    return theta,errors,thetas
+
+def standarize(X):
+    """特征标准化处理
+
+    Args:
+        X 样本集
+    Returns:
+        标准后的样本集
+    """
+    m, n = X.shape
+    # 归一化每一个特征
+    for j in range(n):
+        features = X[:,j]
+        meanVal = features.mean(axis=0)
+        std = features.std(axis=0)
+        if std != 0:
+            X[:, j] = (features-meanVal)/std
+        else:
+            X[:, j] = 0
+    return X
+
+def normalize(X):
+    """特征归一化处理
+
+    Args:
+        X 样本集
+    Returns:
+        归一化后的样本集
+    """
+    m, n = X.shape
+    # 归一化每一个特征
+    for j in range(n):
+        features = X[:,j]
+        minVal = features.min(axis=0)
+        maxVal = features.max(axis=0)
+        diff = maxVal - minVal
+        if diff != 0:
+           X[:,j] = (features-minVal)/diff
+        else:
+           X[:,j] = 0
+    return X
